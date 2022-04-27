97006f220998f33a3d27740392091a9a3ee15b95
ISB-YT <mcpebuildteam@gmail.com>
2022-04-27 12:36:39 -0400

    ENV

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const fetch = require("node-fetch")
+const app = express()
+
+require('dotenv').config()
+
+const cookie = process.env.cookie
+const groupId = process.env.group
+
+app.get("/pending", async (req, res) => {
+    fetch(`https://economy.roblox.com/v1/groups/${groupId}/revenue/summary/day`, {
+        headers: {
+            cookie: `.ROBLOSECURITY=${cookie}`
+        }
+    })
+        .then(result => result.json())
+        .then(json => {
+            return res.json({
+                pending: json.pendingRobux
+            })
+        });
+})
+
+app.listen(process.env.port || 3030)
